2da72e91a0f65fc9d972e76f9c3ae7a09c4acc99
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-11-03 22:09:31 -0300

    add route put

--- src/routes/Item.ts
+++ src/routes/Item.ts
@@ -5,6 +5,7 @@ const itemsRouter = Router()
 
 itemsRouter.get('/items', itemController.listItems)
 itemsRouter.post('/items', itemController.createItem)
+itemsRouter.put('/items/:id', itemController.editItemById)
 itemsRouter.delete('/items/:id', itemController.deleteItemById)
 
 export default itemsRouter
